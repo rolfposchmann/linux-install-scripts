a756f3b673315c80b669d165e945f6257d041b6d
Rolf Poschmann <51391792+rolfposchmann@users.noreply.github.com>
2020-04-04 12:55:32 +0200

    Update ubuntu_full_rolf.sh

--- ubuntu_full_rolf.sh
+++ ubuntu_full_rolf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y git
+sudo apt install -y curl wget git
 
 #Auflösung
 
@@ -20,17 +20,48 @@ sudo apt install -y gnome-tweaks
 #sudo apt-add-repository ppa:tista/adapta
 sudo apt install papirus-icon-theme adapta-gtk-theme breeze-cursor-theme chrome-gnome-shell
 #hier aktivieren: https://extensions.gnome.org/extension/1160/dash-to-panel/
-wget -O ~/PanelConfig https://raw.githubusercontent.com/raspberrypi/linux/rpi-3.2.27/arch/arm/configs/bcmrpi_defconfig
+wget https://raw.githubusercontent.com/rolfposchmann/linux-install-scripts/master/files/ConfigRolfBar -O ~/PanelConfig
 
 
 # Weitere Icons, Themes
-sudo git clone https://github.com/EliverLara/Nordic.git /usr/share/themes/Nordic
-sudo git clone https://github.com/zayronxio/Zafiro-icons.git /usr/share/icons/Zafiro
-sudo apt-get install arc-theme
 
+#Arc Theme
+sudo apt-get install arc-theme
+#Nordic Theme
+sudo git clone https://github.com/EliverLara/Nordic.git /usr/share/themes/Nordic
+
+#Zafiro icons
+sudo git clone https://github.com/zayronxio/Zafiro-icons.git /usr/share/icons/Zafiro
+
+#Paper icons
 mkdir -p ~/themes/icons && cd ~/themes/icons && git clone https://github.com/rolfposchmann/paper-icon-theme.git
 cd paper-icon-theme/
 sudo apt install -y meson
 meson "build" --prefix=/usr
 #install to /usr/
 sudo ninja -C "build" install
+
+#gedit theme
+mkdir -p ~/.local/share/gedit/styles
+wget https://raw.githubusercontent.com/rolfposchmann/linux-install-scripts/master/files/material-theme.xml -O ~/.local/share/gedit/styles/material-theme.xml
+
+#Termial
+
+sudo apt-get install -y zsh 
+sudo apt install -y fonts-powerline
+
+#Nord theme for console
+git clone https://github.com/arcticicestudio/nord-gnome-terminal.git
+cd nord-gnome-terminal/src
+./nord.sh
+
+#More color themes for console
+#https://github.com/Mayccoll/Gogh
+sudo apt-get install -y dconf-cli uuid-runtime
+bash -c  "$(wget -qO- https://git.io/vQgMr)"
+#-> 33 desert
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#nano ~/.zshrc
+#-> ZSH_THEME="agnoster"
+#-> plugins=(git bundler osx rake ruby)
