f3822df70d8df9c3e6c76734601db1c5912efbad
Rolf Poschmann <51391792+rolfposchmann@users.noreply.github.com>
2020-04-16 11:53:09 +0200

    Update themes_icons.sh

--- themes_icons.sh
+++ themes_icons.sh
@@ -38,3 +38,45 @@ sudo ninja -C "build" install
 mkdir -p ~/.local/share/gedit/styles
 wget https://raw.githubusercontent.com/rolfposchmann/linux-install-scripts/master/files/material-theme.xml -O ~/.local/share/gedit/styles/material-theme.xml
 
+#Material-dark (mit Numic Circle)
+https://github.com/nana-4/materia-theme
+
+#Adapta
+sudo apt install adapta-gtk-theme
+#Arc Theme
+sudo apt-get install -y arc-theme
+
+#CBalckdark
+
+#############################
+# Icons
+
+#Nordic Theme
+sudo git clone https://github.com/EliverLara/Nordic.git /usr/share/themes/Nordic
+
+#Zafiro icons
+sudo git clone https://github.com/zayronxio/Zafiro-icons.git /usr/share/icons/Zafiro
+
+#Numix icons
+#sudo add-apt-repository ppa:numix/ppa
+#sudo apt update
+#sudo apt install numix-icon-theme-circle
+sudo apt install -y numix-icon-theme-circle
+
+#paper icons
+mkdir -p ~/.icons && cd ~/.icons && git clone https://github.com/rolfposchmann/paper-icon-theme.git
+cd ~/.icons/paper-icon-theme/
+sudo apt install -y meson
+meson "build" --prefix=/usr
+#install to /usr/share/icons/Paper
+sudo ninja -C "build" install
+
+#vimix-icon-theme
+cd ~/.icons && git clone https://github.com/vinceliuice/vimix-icon-theme.git
+cd vimix-icon-theme/
+./install.sh
+
+#La-capitaine-icon-theme
+cd ~/.icons && git clone https://github.com/keeferrourke/la-capitaine-icon-theme.git
+cd la-capitaine-icon-theme/
+./configure
