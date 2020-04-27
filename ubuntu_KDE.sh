105d49e90f18935b600053dca168c441af98450f
Rolf Poschmann <51391792+rolfposchmann@users.noreply.github.com>
2020-04-27 12:21:46 +0200

    Update ubuntu_KDE.sh

--- ubuntu_KDE.sh
+++ ubuntu_KDE.sh
@@ -2,6 +2,9 @@
 
 #Firefox
 about:config
-layers.acceleration.force-enabled
-browser.tabs.remote.force-enable
---> enable
+layers.acceleration.force-enabled   --> enable
+browser.tabs.remote.force-enable    --> enable
+network.http.pipelining             --> enable    
+network.http.proxy.pipelining       --> enable
+network.dns.disableIPv6             --> enable
+
