362c09293ae85cbbf650ce72e8322c9660c13e6f
Rolf Poschmann <51391792+rolfposchmann@users.noreply.github.com>
2020-08-19 18:33:56 +0200

    Create tilix.sh

---
+++ tilix.sh
@@ -0,0 +1,9 @@
+sudo apt install tilix
+sudo update-alternatives --config x-terminal-emulator
+
+ Auswahl      Pfad                             Priorität Status
+------------------------------------------------------------
+* 0            /usr/bin/gnome-terminal.wrapper   40        automatischer Modus
+  1            /usr/bin/gnome-terminal.wrapper   40        manueller Modus
+  2            /usr/bin/tilix.wrapper            30        manueller Modus
+
